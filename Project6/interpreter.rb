9ec9880e9f640e3b4d15d555a2ae2f7dc9bb357d
mik90 <mik90@pitt.edu>
2018-04-01 20:58:36 -0400

    Fixed more rubocop warnings

--- Project6/interpreter.rb
+++ Project6/interpreter.rb
@@ -2,18 +2,12 @@ require_relative 'token_stack'
 
 class Interpreter
   def initialize(repl, filename)
-    @debug = false
     @operators = ['+', '-', '/', '*']
-    @keywords = %w(print let quit)
+    @keywords = %w[print let quit]
     @line_number = 0
     @repl_mode = repl
-    if repl
-      repl(TokenStack.new, 0)
-    else
-      # Read file array if we're REPLing
-      # Throw error if the file arguments are invalid
-      read_file(filename)
-    end
+    repl(TokenStack.new, 0) if repl
+    read_file(filename) unless repl
   end
 
   def repl(stack, line_number)
@@ -24,31 +18,25 @@ class Interpreter
   end
 
   def read_file(filename)
-    error("File \"#{filename}\" !found") unless File.exist?(filename)
     file = File.open(filename, 'r')
     stack = TokenStack.new # Variables and tokens are reset per file (program)
     file.each_line do |line|
-      # Calls function per line
-      @line_number += 1
       # line.lstrip.empty? Checks if there is any non-whitespace characters
-      stackify_input(@line_number, line, stack) unless line.lstrip.empty?
+      stackify_input(@line_number += 1, line, stack) unless line.lstrip.empty?
     end
   end
 
   def stackify_input(line_number, line, stack)
     line.split.each do |token| # Array-ified line
       unless (token.length == 1 && token.match?(/[[:alpha:]]/)) ||
-             token.match?(/[[:digit:]]+/) ||
-              @keywords.include?(token) ||
+             token.match?(/[[:digit:]]+/) || @keywords.include?(token) ||
              @operators.include?(token)
         # The token is not a number, keyword, or single char
         error(4, stack, line_number, "Unknown keyword #{token}")
       end
       stack.push(token) # Push space-delimited tokens
-      if @operators.include?(stack.top)
-        # Do binary op if we reached an operator
-        result = binary_operation(line_number, stack)
-        stack.push(result) # Push result onto top of stack
+      if @operators.include?(stack.top) # op at top of stack
+        stack.push(binary_operation(line_number, stack)) # Push result on top
       end
     end
     evaluate(line_number, stack)
@@ -72,7 +60,7 @@ class Interpreter
       end
       rhs = stack.get_var(rhs) # Dereference variable
     end
-    if lhs.is_a?(String) 
+    if lhs.is_a?(String)
       if stack.get_var(lhs).nil?
         error(1, stack, line_number, "Variable #{lhs} is not initialized")
       end
@@ -92,56 +80,52 @@ class Interpreter
   end
 
   def evaluate(line_number, stack)
-    # Check for keywords here since binary operations
-    if (stack.bottom.is_a? String) && stack.bottom.eql?('print')
-      # PRINT instruction
-      if stack.size != 1
-        error(3, stack, line_number, 'Operator PRINT applied to empty stack')
+    if stack.bottom.is_a?(String)
+      if stack.bottom.eql?('print')
+        # PRINT instruction
+        if stack.size != 1
+          error(3, stack, line_number,
+                'Operator PRINT applied to empty stack')
+        end
+        result = stack.pop # should be stack[1]
+        unless result.class < Numeric ||
+               result.match?(/[a-z]/) # if result is some type of number
+          error(5, stack, line_number, "Invalid operand \"#{result}\"")
+        end
+        # Dereference variable
+        result = stack.get_var(result) if result.is_a? String
+        if stack.size > 1
+          error(3, stack, line_number,
+                "#{stack.size} elements in stack after evaluation")
+        end
+        puts result unless @repl_mode
+      elsif stack.bottom.eql?('let')
+        result = stack.pop # stack[2]
+        if stack.size != 1
+          error(3, stack, line_number,
+                'Operator LET applied to empty stack')
+        end
+        new_var = stack.pop # stack[1]
+        if stack.size > 1
+          error(3, stack, line_number,
+                "#{stack.size} elements in stack after evaluation")
+        end
+        stack.add_var(new_var, result) # Put new variable into hash table
+      elsif stack.bottom.eql?('quit')
+        exit # QUIT instruction
       end
-      result = stack.pop # should be stack[1]
-      unless result.class < Numeric ||
-             result.match?(/[a-z]/) # if result is some type of number
-        error(5, stack, line_number, "Invalid operand \"#{result}\"")
-      end
-      if result.is_a? String # Dereference variable
-        result = stack.get_var(result)
-      end
-      if stack.size > 1
-        error(3, stack, line_number,
-              "#{stack.size} elements in stack after evaluation")
-      end
-      puts result unless @repl_mode
-    elsif (stack.bottom.is_a? String) && stack.bottom.eql?('let')
-      # LET instruction
-      result = stack.pop # stack[2]
-      if stack.size != 1
-        error(3, stack, line_number,
-              'Operator LET applied to empty stack')
-      end
-      new_var = stack.pop # stack[1]
-      if stack.size > 1
-        error(3, stack, line_number,
-              "#{stack.size} elements in stack after evaluation")
-      end
-      stack.add_var(new_var, result) # Put new variable into hash table
-    elsif (stack.bottom.is_a? String) && stack.bottom.eql?('quit')
-      # QUIT instruction
-      exit
     else
       if stack.empty?
         error(5, stack, line_number, 'Could not evaluate expression')
       end
-      # If there isn't a keyword, then the bottom of the stack should
-      # be a number
+      # If there wasn't a keyword, stack bottom should be a number
       unless stack.size == 1
         error(3, stack, line_number,
               "#{stack.size} elements in stack after evaluation")
       end
       result = stack.bottom
     end
-    if @repl_mode == true
-      puts result # REPL output
-    end
+    puts result if @repl_mode == true
   end
 
   def error(message)
@@ -150,8 +134,9 @@ class Interpreter
   end
 
   def error(error_code, stack, line_number, message)
-    puts "Line #{line_number}: " + message
-    abort("Exit with error code:#{error_code}") unless @repl_mode
+    output = "Line #{line_number}: " + message
+    abort(output + "Exit with error code:#{error_code}") unless @repl_mode
+    puts output
     stack.reset # Clean up our stack
     repl(stack, line_number)
   end
