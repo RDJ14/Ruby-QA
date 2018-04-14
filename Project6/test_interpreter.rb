563aa183c3a998c88538ebb5230875e02ad4a919
RJ <RJ@DESKTOP-TNNUE6E>
2018-04-14 16:18:23 -0400

    Added unit tests

---
+++ Project6/test_interpreter.rb
@@ -0,0 +1,100 @@
+require 'minitest/autorun'
+require_relative 'interpreter'
+
+# Test methods for the interpreter
+class TestInterpreter < Minitest::Test
+  # Test Methods for check_null_operation(lhs, rhs)
+  # => SUCCESS CASES: If either lhs or rhs is nil then true is returned.
+  # =>                If both lhs and rhs are not nil then false
+  # => FAILURE CASES: If one of the parameters is nil and false is returned
+  # =>                If both paramters are not nil and true is returned
+  def test_both_nil
+    terp = Interpreter.new(false, true)
+    assert terp.check_null_operation(nil, nil)
+  end
+
+  def test_one_nil
+    terp = Interpreter.new(false, true)
+    assert terp.check_null_operation('1', nil)
+  end
+
+  def test_both_value
+    terp = Interpreter.new(false, true)
+    refute terp.check_null_operation('1', '1')
+  end
+
+  # Test Methods for binary_operation(line_number, stack)
+  # SUCCESS CASES: The top 3 objects on the stack are popped off and evaluated,
+  #                the result of them is return.
+  # =>             An error is printed if the bottom 2 objects of the stack are
+  #                not operands
+  # =>             An error is printed if the top of the stack is not an
+  #                operator
+  # FAILURE CASES: The program crahsed from mishandling any of the 3 objects
+  # =>             The incorrect value is returned
+  def test_proper_stack
+    stack = TokenStack.new
+    stack.push('4')
+    stack.push('3')
+    stack.push('*')
+    terp = Interpreter.new(false, true)
+
+    result = terp.binary_operation(0, stack)
+    assert_equal 12, result
+  end
+
+  def test_bad_operator
+    stack = TokenStack.new
+    stack.push('4')
+    stack.push('3')
+    stack.push('#')
+    terp = Interpreter.new(false, true)
+
+    result = terp.binary_operation(0, stack)
+    assert_equal 'Error', result
+  end
+
+  def test_too_few_operands
+    stack = TokenStack.new
+    stack.push('4')
+    stack.push('+')
+    terp = Interpreter.new(false, true)
+    result = terp.binary_operation(0, stack)
+    assert_equal 'Error', result
+  end
+
+  def test_no_operator
+    stack = TokenStack.new
+    stack.push('4')
+    stack.push('3')
+    terp = Interpreter.new(false, true)
+    result = terp.binary_operation(0, stack)
+    assert_equal 'Error', result
+  end
+
+  # Test Methods for error(error_code, stack, line_number, message)
+  # SUCCESS CASES: The line number is printed followed by the message followed
+  #                by the error code. The string is returned for tests. Stack is
+  #                is reset for repl mode.
+  # FAILURE CASES: Nothing is returned
+  # =>             The incorrect string is returned
+  # =>             Stack is not reset
+  def test_error_normal
+    stack = TokenStack.new
+    expected = 'Line 0: Testing Exit with error code:0'
+    terp = Interpreter.new(false, true)
+    error = terp.error(0, stack, 0, 'Testing')
+
+    assert_equal expected, error
+  end
+
+  def test_error_stack_rest
+    stack = TokenStack.new
+    stack.push('4')
+    stack.push('3')
+    terp = Interpreter.new(false, true)
+    terp.error(0, stack, 0, 'Testing')
+
+    assert_equal 0, stack.size
+  end
+end
--- Project6/test_token_stack.rb
+++ Project6/test_token_stack.rb
@@ -22,13 +22,13 @@ class TestTokenStack < Minitest::Test
     token_stack.pop
     assert_equal(token_stack.stack.count, 0)
   end
-  
+
   def test_empty_pop
     token_stack = TokenStack.new
     token_stack.pop
     assert_equal(token_stack.stack.count, 0)
   end
- 
+
   # Unit test for TokenStack::get_var
   # Success case: returns a variable
   # Failure case: returns nil
@@ -37,9 +37,60 @@ class TestTokenStack < Minitest::Test
     token_stack.variables['a'] = 3
     assert_equal(token_stack.get_var('a'), 3)
   end
-  
+
   def test_invalid_get_var
     token_stack = TokenStack.new
     assert_nil(token_stack.get_var('a'))
+  end
+
+  # Test Methods for reset()
+  # SUCCESS CASES: The stack is Reset
+  # FAILURE CASES: The stack is left with elements
+  # =>             The stack is deleted
+  def test_reset
+    stack = TokenStack.new
+    stack.push('0')
+    stack.push('1')
+    stack.push('2')
+    stack.push('3')
+    stack.reset
+
+    assert_empty stack
+  end
+
+  # Test methods for size()
+  # SUCCESS CASES: The number of elements in the stack is returned minus the
+  #                keywords
+  # FAILURE CASES: Improper count is returned
+  # =>             nil is returned
+  def test_no_keyword
+    stack = TokenStack.new
+    stack.push('0')
+    stack.push('1')
+    stack.push('2')
+    stack.push('3')
+    stack.push('4')
+    stack.push('5')
+    count = stack.size
+    assert_equal 6, count
+  end
+
+  def test_keywords
+    stack = TokenStack.new
+    stack.push('0')
+    stack.push('1')
+    stack.push('print')
+    stack.push('2')
+    stack.push('3')
+    stack.push('let')
+    stack.push('quit')
+    count = stack.size
+    assert_equal 4, count
+  end
+
+  def test_empty
+    stack = TokenStack.new
+    count = stack.size
+    assert_equal 0, count
   end
 end
